bd0b7cb59617c75526ca41e60af039af2fedf50e
Samantha Diamond <samanthadiamond89@gmail.com>
2016-03-09 07:09:01 +1100

    Tic-Tac-Toe completed

--- js/main.js
+++ js/main.js
@@ -1,80 +1,49 @@
-// playerMove function
-// winner function
-// clear function
-
 var TicTacToe = {
+  numClicks: 0,
   board : [["_", "_", "_"],
           ["_", "_", "_"],
           ["_", "_", "_"]],
-  // winningCombinationsRow : [["x", "x", "x"], ["y", "y", "y"]],
-  // winningCombinationsCol : []
   playerMove : function (row, col, player) {
     this.board[row][col] = player;
   },
-
-
-
-
-
-
-
-
+  gameComplete: function () {
+    swal("Congratulations, you won!", "Ready for another game?");
+    $(".x, .o").removeClass("o").removeClass("x");
+    return this.board = [["_", "_", "_"],["_", "_", "_"],["_", "_", "_"]];
+  },
   checkForWinner : function () {
-    for (var i = 0; i < this.board.length; i++) {
-      if (this.board[i][0] === this.board[i][1] && this.board[i][1] === this.board[i][2]) {
-      return "winner";
+    for (var i = 0; i < 3; i++) {
+      if (this.board[i][0] === this.board[i][1] && this.board[i][1] === this.board[i][2] && this.board[i][0] !== "_") {
+        this.gameComplete('winner');
+        return true;
+      } else if (this.board[0][i] === this.board[1][i] && this.board[1][i] === this.board[2][i] && this.board[0][i] !== "_") {
+        this.gameComplete('winner');
+        return true; // VERTICAL WINS
+      }
     }
-    if (this.board[0][0] === this.board[1][0] && this.board[i][1] === this.board[i][2]) {
-    return "winner";
+
+    if (this.board[0][0] === this.board[1][1] && this.board[1][1] === this.board[2][2] && this.board[2][2] !== "_") {
+      this.gameComplete("winner");
+      return true;
+    } else if (this.board[0][2] === this.board[1][1] && this.board[1][1] === this.board[2][0] && this.board[2][0] !== "_") {
+      this.gameComplete("winner");
+      return true;
     }
   }
- };
-
-
-    // input: none
-    // return: player, none
-  // numClicks : 0,
-
-
-
+};
 
 $(document).ready(function () {
-  $('.box').on('click', function () {
-    // change to 'x' or 'o';
-    $(this).addClass("x");
-
-    // get coordinates
-
-
-    // get player
-      // odd click = x
-      // even click = 0
-      // (nClicks) % 2 === 0;
-      // increment
-
-    // playerMove () to update array
-
-    // checkForWinner() to check if won
-      // if winner => animation or something to identify winner
-      // clear board
-
-
-  })
+  $(".box").on("click", function () {
+    TicTacToe.numClicks += 1;
+    var xCoord = parseInt( $(this).data("x") );
+    var yCoord = parseInt( $(this).data("y") );
+    if (TicTacToe.numClicks % 2 === 0) {
+      $(this).addClass("x");
+      TicTacToe.playerMove(xCoord, yCoord, "x");
+    } else {
+      $(this).addClass("o");
+      TicTacToe.playerMove(xCoord, yCoord, "o");
+    }
+    TicTacToe.checkForWinner();
+  });
 });
-
-
-
-
-
-
-
-
-
-
-
-// var logic
-// var display
-// ["_","_","_"]
-// preso
-// click handlers
-// keep track of move e.g. var change move;
